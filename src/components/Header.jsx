e935c2862555e5a6c1e6cf3a01c651ebc0c16c34
xep97 <pettersen.egil@gmail.com>
2025-11-16 03:02:06 +0100

    added more stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,49 @@ import Image from 'next/image'
 export default function Header() {
   return (
     <>
-        <div>
-            <Image
+        <div className='flex flex-row items-center justify-between'>
+            <div className='flex flex-row items-center justify-items-start'>
+              <Image
                 className="dark:invert"
                 src="/file.svg"
                 alt="Post something"
-                width={100}
+                width={60}
                 height={20}
                 priority
-            />
-            <h1>Post something</h1>
+              />
+              <h1>Post something</h1>
+            </div>
+            
+            <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+              <a
+                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                href="posts"
+                rel="noopener noreferrer"
+              >
+                Read post
+              </a>
+              <a
+                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                href="my-posts"
+                rel="noopener noreferrer"
+              >
+                My posts
+              </a>
+              <a
+                className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+                href="create"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  className="dark:invert"
+                  src="/file.svg"
+                  alt="Vercel logomark"
+                  width={16}
+                  height={16}
+                />
+                New post
+              </a>
+          </div>
         </div>
     </>
   )
