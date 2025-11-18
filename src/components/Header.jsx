5cb4aff7cca980bd405d8c7d75b8e1844738065a
xep97 <pettersen.egil@gmail.com>
2025-11-18 10:07:28 +0100

    finally made single post display work after way too many attempts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,7 @@ export default function Header() {
               </a>
               <a
                 className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-                href="post/762c6994-ad71-4fcf-b1cc-e7d8514b38a2"
+                href="posts/762c6994-ad71-4fcf-b1cc-e7d8514b38a2"
                 rel="noopener noreferrer"
               >
                 Example post
